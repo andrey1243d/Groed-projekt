6f9c621e8c9c4a0b6eb7b431bf81742e4a842a94
Andre1352 <profilprofil91@gmail.com>
2025-09-08 17:53:42 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './pages/Home.vue';
+import Home from './pages/home.vue';
 import register from './pages/registration.vue';
 
 const routes = [
